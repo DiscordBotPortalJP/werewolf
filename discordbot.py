0f331dc5929a6f825e4ff4301539be857444f6f3
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-05-02 20:01:27 +0900

    Feat: ゲーム開始コマンドを定義

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,47 @@
+# TODO
+# オブジェクト
+#   GM：bot
+#   村
+#   参加者
+#       編成テンプレートの定義
+#           4:村村占狼
+#           5:村村村占狼
+#   村人陣営の役職
+#       村人
+#       占い師
+#   人狼陣営の役職
+#       人狼
+#   日数
+#
+# ゲーム進行
+#   参加者の募集
+#   プレイヤー参加
+#   ゲーム開始
+#   役職の割り振り
+#   投票セット
+#   占いセット
+#   襲撃セット
+#   処刑処理
+#   襲撃処理
+#   ゲーム終了判定
+#   ゲーム結果の表示
+#   日付変更処理
+#   占い判定処理
+#   襲撃結果(生存状況)表示
+#       「Aさんが無残な姿で発見されました」
+#   占い結果表示
+
 from discord.ext import commands
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_WEREWOLF_TOKEN']
+bot.game_status = 'nothing'
 
+# ゲーム開始前：nothing
+# 参加者募集中:waiting
+# ゲーム中:playing
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -14,8 +51,14 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+async def create(ctx):
+    if bot.game_status == 'playing':
+        await ctx.send('ゲーム中です')
+        return
+    if bot.game_status == 'waiting':
+        await ctx.send('既に参加者募集中です')
+        return
+    await bot.game_status = 'waiting'
+    await ctx.send('参加者の募集を開始しました')
 
 bot.run(token)

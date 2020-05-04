d86baad2c8b7cbc63b172f5bd7040eb4f97e1946
daima3629 <daima3629@usbc.be>
2020-05-04 14:32:38 +0900

    [Add] joinコマンドの実装

--- cogs/players.py
+++ cogs/players.py
@@ -5,14 +5,25 @@ from discord.ext import commands
 class PlayersCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bot.players = []
 
     # ゲームに参加するコマンド
     # bot.players に Player オブジェクトを格納する
     # ステータスが参加者募集中かをチェックする
     # 既に参加しているメンバーかをチェックする
     @commands.command()
-    def join(self, ctx):
-        pass
+    async def join(self, ctx):
+        if self.bot.game_status == "nothing":
+            return await ctx.send("現在ゲームはありません。")
+        elif self.bot.game_status == "playing":
+            return await ctx.send("現在ゲーム進行中です。")
+        
+        user = ctx.author
+        if user in self.bot.players:
+            return await ctx.send("すでにゲームに参加しています。")
+        
+        self.bot.players.append(user)
+        await ctx.send(f"{user.nick if user.nick else user.name}さんが参加しました。")
 
 
 def setup(bot):

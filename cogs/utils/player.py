2885fb4d9eedbdcc92fa694a4116ad97e1dff05e
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-05-05 19:59:20 +0900

    Feat: 各種指定先のプロパティを定義

--- cogs/utils/player.py
+++ cogs/utils/player.py
@@ -3,6 +3,9 @@ class Player():
     def __init__(self, discord_id):
         self.id = discord_id  # Discord ID(int)
         self.role = '村人'  # 役職名(str)
+        self.vote_target = None  # ?Player
+        self.raid_target = None  # ?Player
+        self.fortune_target = None  # ?Palyer
 
     def set_role(self, role):
         self.role = role

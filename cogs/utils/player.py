b51388eebbd447f7789a57bdd780b8d343742fe2
Sumito Izumita <sumito@izumita.com>
2020-05-05 12:34:58 +0900

    プロパティの追加
    [Add] プレイヤーが死んでいるかのプロパティを追加

--- cogs/utils/player.py
+++ cogs/utils/player.py
@@ -3,6 +3,7 @@ class Player():
     def __init__(self, discord_id):
         self.id = discord_id  # Discord ID(int)
         self.role = '村人'  # 役職名(str)
+        self.is_dead = False
 
     def set_role(self, role):
         self.role = role

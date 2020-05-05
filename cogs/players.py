3578a432c4ba4ea94065dd72c30fd5d6efec9a18
daima3629 <daima3629@usbc.be>
2020-05-05 14:51:57 +0900

    [Fix] flake8くんに従って空行など修正

--- cogs/players.py
+++ cogs/players.py
@@ -24,7 +24,7 @@ class PlayersCog(commands.Cog):
         player = Player(member.id)
         self.bot.players.append(player)
         await ctx.send(f"{member.mention}さんが参加しました。")
-    
+
     @commands.command()
     async def leave(self, ctx):
         if self.bot.game_status == "nothing":
@@ -32,7 +32,7 @@ class PlayersCog(commands.Cog):
         elif self.bot.game_status == "playing":
             return await ctx.send("既にゲームが始まっているため退出できません。")
         member = ctx.author
-        exists = False # Playerがself.bot.playersに存在しているか
+        exists = False       # Playerがself.bot.playersに存在しているか
         for p in self.bot.players:
             if member.id == p.id:
                 exists = True
@@ -42,7 +42,6 @@ class PlayersCog(commands.Cog):
             return await ctx.send("ゲームに参加していません。")
         self.bot.players.remove(player)
         await ctx.send("ゲームから退出しました。")
-
 
 
 def setup(bot):

23a848a344f3434386dcde2dfa0872deedef6104
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-05-06 16:33:26 +0900

    Fix: pytestのparametrizeを利用してテストパターンを配列で用意するように

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,20 +1,36 @@
+import pytest
 from cogs.status import is_village_win, is_werewolf_win
 from cogs.utils.player import Player
 
 
-def test_is_village_win():
+village_pattern = [
+    ('村村村', True),
+    ('村占狼村', False),
+    ('村村狼狼', False),
+]
+
+werewolf_pattern = [
+    ('村村村', False),
+    ('村占狼村', False),
+    ('村村狼狼', True),
+]
+
+
+@pytest.mark.parametrize('roles, expect', village_pattern)
+def test_is_village_win(roles, expect):
     players = []
-    for role in '村村村':
+    for role in roles:
         player = Player(1)
         player.set_role(role)
         players.append(player)
-    assert is_village_win(players) is True
+    assert is_village_win(players) is expect
 
 
-def test_is_werewolf_win():
+@pytest.mark.parametrize('roles, expect', werewolf_pattern)
+def test_is_werewolf_win(roles, expect):
     players = []
-    for role in '村村狼狼':
+    for role in roles:
         player = Player(1)
         player.set_role(role)
         players.append(player)
-    assert is_werewolf_win(players) is True
+    assert is_werewolf_win(players) is expect

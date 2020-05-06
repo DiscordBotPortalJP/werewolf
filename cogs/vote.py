37d33b658a1c1abd877491cec87247031c412770
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-05-06 17:11:33 +0900

    Fix: プロパティとメソッドの整理

--- cogs/vote.py
+++ cogs/vote.py
@@ -4,24 +4,17 @@ from discord.ext import commands
 class Vote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.voted_player = None  # 処刑される人
-        self.fortuned_player = None  # 占いをされる人
-        self.killed_player = None  # 人狼に殺される人
-
-    async def fortune(self, player):
-        """占い"""
-        pass
-
-    async def wolf(self, players):
-        """人狼"""
-        pass
-
-    async def player_vote(self, players):
-        """処刑"""
-        pass
 
     @commands.command()
     async def vote(self, ctx):
+        pass
+
+    @commands.command()
+    async def raid(self, ctx):
+        pass
+
+    @commands.command()
+    async def fortune(self, ctx):
         pass
 
 
